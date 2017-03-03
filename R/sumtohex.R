e119509613d638cf6bab2a6ae0a9424f486b074e
Wazeer Zulfikar <wazeer.zulfikar@gmail.com>
2017-03-03 12:39:53 +0530

    First commit

---
+++ R/sumtohex.R
@@ -0,0 +1,15 @@
+#' A Sum to Hex Function
+#'
+#' Adds two numbers and returns result in hexadecimal string.
+#' @param num1 A number.
+#' @param num2 A number.
+#' @return The sum of \code{num1} and \code{num2} in hexadecimal string.
+#' @examples
+#' sumtohex(5, 6) = "b"
+#' sumtohex(230, 420) = "28a"
+
+sumtohex <- function(num1,num2) {
+  return(as.hexmode(num1 + num2))
+}
+
+
